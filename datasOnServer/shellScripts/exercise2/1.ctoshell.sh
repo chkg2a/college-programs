029e0d28f47db60265d4f44b0f9aae171aefc9b9
chkg2a <xshalltearvamps@gmail.com>
2024-09-05 15:39:44 +0530

    yoolo

--- datasOnServer/shellScripts/exercise2/1.ctoshell.sh
+++ datasOnServer/shellScripts/exercise2/1.ctoshell.sh
@@ -2,17 +2,20 @@
 
 read -p "Enter the lower limit : " L
 read -p "Enter the upper limit : " N
+read -p "Enter the steps to jump : " S
 
+g++ 1.c -o output;
 cProg=$(./output $N);
 
 fileName="newFile"
 
-rm -f $fileName
-touch $fileName
+> $fileName
 
 while IFS= read -r line; do
-  if [ $L -gt $line ]; then
+  if [ $L -gt $line ] || [ $(( line % S )) != 0 ]; then
     continue
   fi
   echo $line >> $fileName
 done <<< "$cProg"
+
+rm -rf output
--- datasOnServer/shellScripts/exercise2/3.month.sh
+++ datasOnServer/shellScripts/exercise2/3.month.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export month="August"
+set -- $(date +%B)
 
-echo $(set | grep month | awk -F= '{print $3}' | awk '{print $1}')
+echo $1
